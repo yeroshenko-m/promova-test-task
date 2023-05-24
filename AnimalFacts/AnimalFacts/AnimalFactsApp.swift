7e396b36056b50db27912ab5a6632f1bb9a938ef
Mykhailo Yeroshenko <yeroshenko.m@icloud.com>
2023-05-25 00:19:20 +0300

    Created the project

---
+++ AnimalFacts/AnimalFacts/AnimalFactsApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimalFactsApp.swift
+//  AnimalFacts
+//
+//  Author:  Mykhailo Yeroshenko
+//  Created: 25.05.2023
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
