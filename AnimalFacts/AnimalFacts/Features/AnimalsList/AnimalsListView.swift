cd0149489d7b4f5292b572a39a69cc348ee60994
Mykhailo Yeroshenko <yeroshenko.m@icloud.com>
2023-05-25 14:26:52 +0300

    Implemented basic AnimalsList logic and view

--- AnimalFacts/AnimalFacts/Features/AnimalsList/AnimalsListView.swift
+++ AnimalFacts/AnimalFacts/Features/AnimalsList/AnimalsListView.swift
@@ -10,13 +10,61 @@ import ComposableArchitecture
 import SwiftUI
 
 struct AnimalsListView: View {
+    let store: StoreOf<AnimalsList>
+
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Constants.backgroundColor.ignoresSafeArea()
+            content
+        }
+    }
+
+    @ViewBuilder
+    private var content: some View {
+        WithViewStore(self.store) { $0 } content: { viewStore in
+            ZStack {
+                if let error = viewStore.error {
+                    Text(error.localizedDescription)
+                } else if viewStore.isLoading {
+                    ProgressView()
+                } else {
+                    animalsList(with: viewStore)
+                }
+            }
+            .onAppear { viewStore.send(.fetchAnimals) }
+        }
+    }
+
+    @ViewBuilder
+    private func animalsList(with viewStore: ViewStoreOf<AnimalsList>) -> some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(spacing: Constants.rowsVerticalPadding) {
+                ForEachStore(
+                    self.store.scope(
+                        state: \.rows,
+                        action: AnimalsList.Action.row(index:action:)
+                    )
+                ) { rowStore in
+                    AnimalRowView(store: rowStore)
+                }
+            }
+            .padding(Constants.rowsHorizontalPadding)
+        }
+        .navigationTitle("Select a category")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+extension AnimalsListView {
+    struct Constants {
+        static let rowsHorizontalPadding: CGFloat = 16.0
+        static let rowsVerticalPadding: CGFloat = 16.0
+        static let backgroundColor: Color = Color(hexString: "#BEC8FF")
     }
 }
 
 struct AnimalsListView_Previews: PreviewProvider {
     static var previews: some View {
-        AnimalsListView()
+        EmptyView()
     }
 }
